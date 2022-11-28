ce8d66932e80367b70cb1577a62b399e1b6b7166
theHesoyam <reviakindi@gmail.com>
2022-11-28 11:38:58 +0200

    add nvim

---
+++ nvim/init.lua
@@ -0,0 +1,14 @@
+require("thehesoyam.plugins-setup")
+require("thehesoyam.core.options")
+require("thehesoyam.core.keymaps")
+require("thehesoyam.core.colorscheme")
+require("thehesoyam.plugins.comment")
+-- require("thehesoyam.plugins.nvim-tree") -- redandant because there is an oprigin menu
+require("thehesoyam.plugins.nvim-cmp")
+require("thehesoyam.plugins.lualine")
+require("thehesoyam.plugins.lsp.mason") -- :Mason
+require("thehesoyam.plugins.lsp.lspsaga")
+require("thehesoyam.plugins.lsp.lspconfig")
+require("thehesoyam.plugins.lsp.null-ls")
+require("thehesoyam.plugins.autopairs")
+require("thehesoyam.plugins.treesitter")
