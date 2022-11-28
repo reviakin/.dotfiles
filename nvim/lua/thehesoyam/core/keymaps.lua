7b2196d6dc937b3bb3e18c5513869e9313306f37
TheHesoyam <thehesoyam@TheHesoyams-MacBook-Pro.local>
2022-11-28 23:12:50 +0200

    add fugitive

--- nvim/lua/thehesoyam/core/keymaps.lua
+++ nvim/lua/thehesoyam/core/keymaps.lua
@@ -4,26 +4,26 @@ local keymap = vim.keymap -- for conciseness
 
 -- general keymaps
 keymap.set("i", "jk", "<ESC>") -- escape from insert mode
-keymap.set('n', '<leader>nh', ':nohl<CR>') -- clear search result
-keymap.set('n', 'x', '"_x') -- don't copy on delete
-keymap.set('n', '<leader>-', '<C-x>') -- decrease a number
-keymap.set('n', '<leader>+', '<C-a>') -- increase a number
+keymap.set("n", "<leader>nh", ":nohl<CR>") -- clear search result
+keymap.set("n", "x", '"_x') -- don't copy on delete
+keymap.set("n", "<leader>-", "<C-x>") -- decrease a number
+keymap.set("n", "<leader>+", "<C-a>") -- increase a number
 
-keymap.set('n', '<leader>sv', '<C-w>v') -- split window vertically
-keymap.set('n', '<leader>sh', '<C-w>s') -- split window horizontally
-keymap.set('n', '<leader>se', '<C-w>=') -- make split windows equal width
-keymap.set('n', '<leader>sx', ':close<CR>') -- close current split window
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
 
-keymap.set('n', '<leader>to', ":tabnew<CR>") --open new tab
-keymap.set('n', '<leader>tx', ":tabclose<CR>") --close new tab
-keymap.set('n', '<leader>tn', ":tabn<CR>") --go to the next tab
-keymap.set('n', '<leader>tp', ":tabp<CR>") --go to the prev tab
+keymap.set("n", "<leader>to", ":tabnew<CR>") --open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") --close new tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") --go to the next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") --go to the prev tab
 
-keymap.set('n', '<leader>pv', '<cmd>Ex<CR>')
+keymap.set("n", "<leader>pv", "<cmd>Ex<CR>")
 
 -- plugin keymaps
 -- vim-maxiizers
-keymap.set('n', '<leader>sm', ":MaximizerToggle<CR>")
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
 
 -- nvim-tree
 -- keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>')
@@ -35,8 +35,14 @@ keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>") -- find string u
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>") -- list open buffers in current neovim instance
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>") -- list available help tags
 
--- telescope git commands (not on youtube nvim video)
+-- replaced with fugitive -- telescope git commands (not on youtube nvim video)
 keymap.set("n", "<leader>gc", "<cmd>Telescope git_commits<cr>") -- list all git commits (use <cr> to checkout) ["gc" for git commits]
 keymap.set("n", "<leader>gfc", "<cmd>Telescope git_bcommits<cr>") -- list git commits for current file/buffer (use <cr> to checkout) ["gfc" for git file commits]
 keymap.set("n", "<leader>gb", "<cmd>Telescope git_branches<cr>") -- list git branches (use <cr> to checkout) ["gb" for git branch]
-keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+-- keymap.set("n", "<leader>gs", "<cmd>Telescope git_status<cr>") -- list current changes per file with diff preview ["gs" for git status]
+
+-- fugitive
+keymap.set("n", "<leader>gs", "<cmd>:Git<CR>")
+keymap.set("n", "<leader>ga", "<cmd>Git fetch --all<CR>")
+keymap.set("n", "<leader>dj", ":diffget //2<CR>")
+keymap.set("n", "<leader>dk", ":diffget //3<CR>")
--- nvim/lua/thehesoyam/plugins-setup.lua
+++ nvim/lua/thehesoyam/plugins-setup.lua
@@ -1,87 +1,86 @@
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-    vim.cmd [[packadd packer.nvim]]
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
-  use 'wbthomason/packer.nvim'
+return require("packer").startup(function(use)
+	use("wbthomason/packer.nvim")
 
-  use 'nvim-lua/plenary.nvim' -- lua plugins that many plugins use
+	use("nvim-lua/plenary.nvim") -- lua plugins that many plugins use
 
-  use 'gruvbox-community/gruvbox' -- prefered theme
+	use("gruvbox-community/gruvbox") -- prefered theme
 
-  use 'christoomey/vim-tmux-navigator' -- tmux & splited window navigation
-  use "szw/vim-maximizer" -- maximazis and restores current window
+	use("christoomey/vim-tmux-navigator") -- tmux & splited window navigation
+	use("szw/vim-maximizer") -- maximazis and restores current window
 
-  use 'tpope/vim-surround' -- ysw' - wrappes a word to ' cs'" - replace ' with " 
-  use 'vim-scripts/ReplaceWithRegister' -- replace coppied by grw
+	use("tpope/vim-surround") -- ysw' - wrappes a word to ' cs'" - replace ' with "
+	use("vim-scripts/ReplaceWithRegister") -- replace coppied by grw
 
-  use 'numToStr/Comment.nvim' -- gcc - comment a lint gc10j/k -commnet 10 lines above/below
+	use("numToStr/Comment.nvim") -- gcc - comment a lint gc10j/k -commnet 10 lines above/below
 
-  -- use 'nvim-tree/nvim-tree.lua' -- file explorer
-  -- use 'nvim-tree/nvim-web-devicons' -- icons
+	-- use 'nvim-tree/nvim-tree.lua' -- file explorer
+	-- use 'nvim-tree/nvim-web-devicons' -- icons
 
+	use("nvim-lualine/lualine.nvim") -- statusline
 
-  use 'nvim-lualine/lualine.nvim' -- statusline
+	-- fuzzy finding w/ telescope
+	use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
+	use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
 
-   -- fuzzy finding w/ telescope
-  use({ "nvim-telescope/telescope-fzf-native.nvim", run = "make" }) -- dependency for better sorting performance
-  use({ "nvim-telescope/telescope.nvim", branch = "0.1.x" }) -- fuzzy finder
+	-- autocompletion
+	use("hrsh7th/nvim-cmp") -- completion plugin
+	use("hrsh7th/cmp-buffer") -- source for text in buffer
+	use("hrsh7th/cmp-path") -- source for file system paths
 
-  -- autocompletion
-  use("hrsh7th/nvim-cmp") -- completion plugin
-  use("hrsh7th/cmp-buffer") -- source for text in buffer
-  use("hrsh7th/cmp-path") -- source for file system paths
+	-- snippets
+	use("L3MON4D3/LuaSnip") -- snippet engine
+	use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+	use("rafamadriz/friendly-snippets") -- useful snippets
 
--- snippets
-  use("L3MON4D3/LuaSnip") -- snippet engine
-  use("saadparwaiz1/cmp_luasnip") -- for autocompletion
-  use("rafamadriz/friendly-snippets") -- useful snippets
+	-- managing & installing lsp servers, linters & formatters
+	use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
+	use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
 
-  -- managing & installing lsp servers, linters & formatters
-  use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
-  use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
+	-- configuring lsp servers
+	use("neovim/nvim-lspconfig") -- easily configure language servers
+	use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
+	use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
+	use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
+	use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
 
-  -- configuring lsp servers
-  use("neovim/nvim-lspconfig") -- easily configure language servers
-  use("hrsh7th/cmp-nvim-lsp") -- for autocompletion
-  use({ "glepnir/lspsaga.nvim", branch = "main" }) -- enhanced lsp uis
-  use("jose-elias-alvarez/typescript.nvim") -- additional functionality for typescript server (e.g. rename file & update imports)
-  use("onsails/lspkind.nvim") -- vs-code like icons for autocompletion
+	-- formatting & linting
+	use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
+	use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
 
-  -- formatting & linting
-  use("jose-elias-alvarez/null-ls.nvim") -- configure formatters & linters
-  use("jayp0521/mason-null-ls.nvim") -- bridges gap b/w mason & null-ls
+	-- treesitter configuration
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = function()
+			local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
+			ts_update()
+		end,
+	})
 
-  -- treesitter configuration
-  use({
-    "nvim-treesitter/nvim-treesitter",
-    run = function()
-      local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-      ts_update()
-    end,
-  })
+	-- auto closing
+	use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
+	use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
 
-  -- auto closing
-  use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
-  use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+	-- git integration
+	use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
-  -- git integration
-  use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
+	use("tpope/vim-fugitive")
 
-  -- Automatically set up your configuration after cloning packer.nvim
-  -- Put this at the end after all plugins
-  if packer_bootstrap then
-    require('packer').sync()
-  end
+	-- Automatically set up your configuration after cloning packer.nvim
+	-- Put this at the end after all plugins
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
-
-
