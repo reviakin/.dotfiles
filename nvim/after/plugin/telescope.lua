b870f74115dea3af075abd51c402f2c817fc9c45
TheHesoyam <reviakindi@gmail.com>
2023-01-27 15:27:08 +0200

    add getting branches and stashes

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,8 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {}) -- all file search
+vim.keymap.set('n', '<leader>pg', builtin.git_branches, {}) -- all brnaches
+vim.keymap.set('n', '<leader>pgs', builtin.git_stash, {}) -- all stashes
 vim.keymap.set('n', '<C-p>', builtin.git_files, {}) -- git file search
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input('Grep > ') })
-end) 
+  builtin.grep_string({ search = vim.fn.input('Grep > ') })
+end)
