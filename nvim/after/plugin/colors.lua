e8a57312007c1e19b87d682517bebe3cda67b741
TheHesoyam <reviakindi@gmail.com>
2023-08-30 11:36:46 +0200

    remove redundant

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,12 +1,11 @@
 require('rose-pine').setup({
 	--- @usage 'auto'|'main'|'moon'|'dawn'
 	variant = 'dawn',
-  -- ваываыаыасо
 	--- @usage 'main'|'moon'|'dawn'
 	dark_variant = 'main',
-	bold_vert_split = true,
+	bold_vert_split = false,
 	dim_nc_background = false,
-	disable_background = false,
+	disable_background = true,
 	disable_float_background = false,
 	disable_italics = false,
 
