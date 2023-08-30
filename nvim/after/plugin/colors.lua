5e8206fbaf527bfbc9c1f75ed02aefa3bd8bda9f
TheHesoyam <reviakindi@gmail.com>
2023-08-30 11:52:40 +0200

    remove redunant

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,61 +1,58 @@
 require('rose-pine').setup({
-	--- @usage 'auto'|'main'|'moon'|'dawn'
-	variant = 'dawn',
-	--- @usage 'main'|'moon'|'dawn'
-	dark_variant = 'main',
-	bold_vert_split = false,
-	dim_nc_background = false,
-	disable_background = true,
-	disable_float_background = false,
-	disable_italics = false,
+  --- @usage 'auto'|'main'|'moon'|'dawn'
+  variant = 'dawn',
+  --- @usage 'main'|'moon'|'dawn'
+  dark_variant = 'main',
+  bold_vert_split = false,
+  dim_nc_background = false,
+  disable_background = true,
+  disable_float_background = false,
+  disable_italics = false,
 
-	--- @usage string hex value or named color from rosepinetheme.com/palette
-	groups = {
-		background = 'base',
-		background_nc = '_experimental_nc',
-		panel = 'surface',
-		panel_nc = 'base',
-		border = 'highlight_med',
-		comment = 'muted',
-		link = 'iris',
-		punctuation = 'subtle',
+  --- @usage string hex value or named color from rosepinetheme.com/palette
+  groups = {
+    background = 'base',
+    background_nc = '_experimental_nc',
+    panel = 'surface',
+    panel_nc = 'base',
+    border = 'highlight_med',
+    comment = 'muted',
+    link = 'iris',
+    punctuation = 'subtle',
 
-		error = 'love',
-		hint = 'iris',
-		info = 'foam',
-		warn = 'gold',
+    error = 'love',
+    hint = 'iris',
+    info = 'foam',
+    warn = 'gold',
 
-		headings = {
-			h1 = 'iris',
-			h2 = 'foam',
-			h3 = 'rose',
-			h4 = 'gold',
-			h5 = 'pine',
-			h6 = 'foam',
-		}
-		-- or set all headings at once
-		-- headings = 'subtle'
-	},
+    headings = {
+      h1 = 'iris',
+      h2 = 'foam',
+      h3 = 'rose',
+      h4 = 'gold',
+      h5 = 'pine',
+      h6 = 'foam',
+    }
+    -- or set all headings at once
+    -- headings = 'subtle'
+  },
 
-	-- Change specific vim highlight groups
-	-- https://github.com/rose-pine/neovim/wiki/Recipes
-	highlight_groups = {
-		ColorColumn = { bg = 'rose' },
+  -- Change specific vim highlight groups
+  -- https://github.com/rose-pine/neovim/wiki/Recipes
+  highlight_groups = {
+    ColorColumn = { bg = 'rose' },
 
-		-- Blend colours against the "base" background
-		CursorLine = { bg = 'foam', blend = 10 },
-		StatusLine = { fg = 'love', bg = 'love', blend = 10 },
+    -- Blend colours against the "base" background
+    CursorLine = { bg = 'foam', blend = 10 },
+    StatusLine = { fg = 'love', bg = 'love', blend = 10 },
 
-		-- By default each group adds to the existing config.
-		-- If you only want to set what is written in this config exactly,
-		-- you can set the inherit option:
-		Search = { bg = 'gold', inherit = false },
-	}
+    -- By default each group adds to the existing config.
+    -- If you only want to set what is written in this config exactly,
+    -- you can set the inherit option:
+    Search = { bg = 'gold', inherit = false },
+  }
 })
 
--- Set colorscheme after options
-vim.cmd('colorscheme rose-pine')
---
 
 function ColorMyPencils(color)
   color = color or 'rose-pine'
@@ -63,12 +60,6 @@ function ColorMyPencils(color)
 
   vim.api.nvim_set_hl(0, "Normal", { bg = 'none' })
   vim.api.nvim_set_hl(0, "NormalFloat", { bg = 'none' })
-
-  --[[
-  vim.cmd('highlight Normal guibg=none')
-  vim.cmd('highlight NonText guibg=none')
-  ]]--
 end
 
--- ColorMyPencils("rose-pine")
-
+ColorMyPencils()
